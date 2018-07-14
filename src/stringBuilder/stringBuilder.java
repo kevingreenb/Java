172ded614f3e0be9c4a404c636e492013fe3b762
KGreen <kevin.e.green@hotmail.com>
2018-07-14 02:58:12 -0500

    String Builder case

--- src/stringBuilder/stringBuilder.java
+++ src/stringBuilder/stringBuilder.java
@@ -10,15 +10,24 @@ package stringBuilder;
  * @author KevinGreen
  */
 public class stringBuilder {
-    
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         StringBuilder a = new StringBuilder("abc");
-        StringBuilder b  = a.append("de");
-        
+        StringBuilder b = a.append("de");
+
         System.out.println(a);
         System.out.println(b);
-        
+
+        StringBuilder s1 = new StringBuilder("Java");
+        String s2 = "Love";
+        String s3 = "Gato";
+        s1.append(s2);
+        s1.append("Gato");
+        s1.substring(4);
+        int foundAt = s1.indexOf("to");
+        System.out.println(foundAt);
+
     }
-    
+
 }
